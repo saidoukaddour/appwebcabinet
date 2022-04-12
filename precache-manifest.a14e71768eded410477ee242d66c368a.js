c7852847aca1a1f5ac0d9cd1eedb4c7156226c13
saidoukaddour <medsaidou69@gmail.com>
2022-04-12 02:28:48 +0200

    Updates

---
+++ precache-manifest.a14e71768eded410477ee242d66c368a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "657baa41c18107b64e62",
+    "url": "/appwebcabinet/static/css/main.f7e8b9f3.chunk.css"
+  },
+  {
+    "revision": "657baa41c18107b64e62",
+    "url": "/appwebcabinet/static/js/main.657baa41.chunk.js"
+  },
+  {
+    "revision": "dca74a035a2640b5084a",
+    "url": "/appwebcabinet/static/js/1.dca74a03.chunk.js"
+  },
+  {
+    "revision": "1dee23ebd7d10e401073",
+    "url": "/appwebcabinet/static/js/2.1dee23eb.chunk.js"
+  },
+  {
+    "revision": "61e8e6d52ff88258b324",
+    "url": "/appwebcabinet/static/js/runtime~main.61e8e6d5.js"
+  },
+  {
+    "revision": "f9556c2074ea7e3e2a471fe1fa52aab4",
+    "url": "/appwebcabinet/static/media/image2.f9556c20.jpg"
+  },
+  {
+    "revision": "07b1a5e9248d596877c03cb7c948c84a",
+    "url": "/appwebcabinet/static/media/image3.07b1a5e9.jpg"
+  },
+  {
+    "revision": "fb31fc9a94cb2e10e7c3acfff91aa9fe",
+    "url": "/appwebcabinet/static/media/logo.fb31fc9a.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/appwebcabinet/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "c3f52ad456955a59505cfa03966dc422",
+    "url": "/appwebcabinet/static/media/image5.c3f52ad4.jpg"
+  },
+  {
+    "revision": "300566172c53c703e5a65d1071ab61b9",
+    "url": "/appwebcabinet/index.html"
+  }
+];
